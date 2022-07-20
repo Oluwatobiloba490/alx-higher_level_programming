192fddac542bbb67203041c65cf9811b0d5c948c
Oluwatobiloba490 <oluwatobilobafred@gmail.com>
2022-07-20 22:30:35 +0100

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Square:
+   
+    
+    def __init__(self, size=0):
+        if type(size) != int:
+           raise TypeError("size must be an integer")
+        elif 
+        
+        
+        self.__size = size
