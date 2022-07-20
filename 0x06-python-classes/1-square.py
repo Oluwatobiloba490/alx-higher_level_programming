0b70de1dd24c8a10c8151a3dde43fcd35f57c352
Oluwatobiloba490 <oluwatobilobafred@gmail.com>
2022-07-20 21:51:46 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,6 +6,6 @@ class Square:
 
       def __init__(self, size):
 
-  ''"I guess this can be here also'''
+  '''I guess this can be here also I'll correct this when i get a hang of it'''
 
            self.__size=size
