01f6d895853606c6439e85a9d87258a81327aade
Oluwatobiloba490 <oluwatobilobafred@gmail.com>
2022-07-20 21:56:34 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
 """This modulle defines a square and initialises it"""
+
 class Square:
+      
 """Square Implementation"""
       def __init__(self, size):
            self.__size=size
