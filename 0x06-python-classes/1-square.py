8eaf63af144f15061d6148cd64780ea493fceb4e
Oluwatobiloba490 <oluwatobilobafred@gmail.com>
2022-07-20 17:12:09 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+
+  '''We are here again with our documentation'''
+
+      def __init__(self, size):
+
+  ''"I guess this can be here also'''
+
+           self.__size=size
